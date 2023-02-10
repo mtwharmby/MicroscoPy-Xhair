21491788d1cc4161d13182172bf5038e77d1d0e3
Michael Wharmby <mtwharmby@gmail.com>
2023-02-10 17:33:53 +0100

    Initial commit of launch.py
    This is for ease of testing.

---
+++ launch.py
@@ -0,0 +1,10 @@
+import logging
+
+import microscopyxhair as mpx
+
+"""
+A simple launcher module to allow easy testing of the UI in development.
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+mpx.main()
