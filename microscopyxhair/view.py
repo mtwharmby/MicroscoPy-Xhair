314ba5800468aa4383969e5c821779c8f45fed50
Michael Wharmby <mtwharmby@gmail.com>
2023-03-06 20:38:11 +0100

    Moved CameraPanel to separate module.

--- microscopyxhair/view.py
+++ microscopyxhair/view.py
@@ -1,9 +1,7 @@
-import logging
-
 import wx
 
-from .model_controller import (DataModel, CaptureController,
-                               CrosshairController)
+from .model_controller import DataModel, CaptureController
+from .camera_view import CameraPanel
 
 
 class CameraIdChooser(wx.Dialog):
@@ -53,77 +51,6 @@ class CameraIdChooser(wx.Dialog):
 
     def OnCancel(self, evt):
         self.Destroy()
-
-
-class CameraPanel(wx.Panel):
-
-    def __init__(self, parent, model: DataModel, cap_ctrl: CaptureController,
-                 *args, **kw):
-        super().__init__(parent, *args, **kw)
-        self.logger = logging.getLogger(__name__)
-
-        self.model: DataModel = model
-        self.capture_ctrl: CaptureController = cap_ctrl
-        self.xhair_ctrl: CrosshairController = CrosshairController(self.model)
-
-        self.init_ui()
-
-    def init_ui(self):
-        # Following line is *crucial* to avoid flickering!
-        self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
-        frame = self.get_frame()
-        self.init_size(frame)
-
-        self.timer = wx.Timer(self)
-        self.timer.Start(int(1000./self.model.framerate))
-
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_TIMER, self.NextFrame)
-
-        self.Bind(wx.EVT_LEFT_UP, self.OnLeftClick)
-
-    def init_size(self, frame):
-        height, width = frame.shape[:2]
-        self.bmp = wx.Bitmap.FromBuffer(width, height, frame)
-
-        self.SetSize((width, height))
-        self.SetMinSize((width, height))
-        self.logger.debug(f"Camera frame size changed to: {(width, height)}")
-
-    def get_frame(self):
-        if not self.model.capture_active:
-            raise RuntimeError("Capture not started")
-        ret, frame = self.capture_ctrl.capture.read()
-
-        if not ret:
-            raise RuntimeError("Cannot get frame")
-
-        return self.xhair_ctrl.draw_crosshair(frame)
-
-    def OnPaint(self, evt):
-        dc = wx.BufferedPaintDC(self, self.bmp)
-        dc.DrawBitmap(self.bmp, 0, 0)
-
-    def NextFrame(self, event):
-        if not self.model.capture_active:
-            return
-        frame = self.get_frame()
-        if (
-            frame.shape[0] != self.GetSize().GetHeight()
-            and frame.shape[1] != self.GetSize().GetWidth()
-        ):
-            self.init_size(frame)
-            wx.PostEvent(self.Parent, wx.EVT_SIZE)
-
-        self.bmp.CopyFromBuffer(frame)
-        self.Refresh()
-
-    def OnLeftClick(self, evt: wx.MouseEvent):
-        self.xhair_ctrl.recentre_crosshair(
-            evt.GetPosition(), self.GetSize().Get()
-        )
-
-        self.Refresh()
 
 
 class MainWindow(wx.Frame):
