7ac4fc8287821681f25ef9c336ca9c6026dab2bf
Michael Wharmby <mtwharmby@gmail.com>
2023-04-30 19:10:47 +0100

    Updates run_pyinstallter
    Creates a zip file containing the directory. Includes function to copy
    artifacts to a redist directory.

--- run_pyinstaller.py
+++ run_pyinstaller.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 import shutil
+import zipfile
+
 import PyInstaller.__main__
+
+
+REPO_ROOT = Path(".").resolve()
+DIST_PATH = REPO_ROOT / "dist"
+REDIST_PATH = REPO_ROOT / "redist"
 
 
 def run_pyi(root: Path, spec_file: str):
@@ -12,26 +19,53 @@ def run_pyi(root: Path, spec_file: str):
     PyInstaller.__main__.run([str(spec_path)])
 
 
-def main():
-    repo_root = Path(".").resolve()
-    dist_path = repo_root / "dist"
-    redist_path = repo_root / "redist"
+def zip_dir(dir: Path, zip_file: zipfile.ZipFile, pre_dirs: int = 0):
+    for fsobj in dir.iterdir():
+        print(fsobj)  # TODO Logging!
+        if fsobj.is_dir():
+            zip_dir(fsobj, zip_file, pre_dirs)
+        else:
+            zip_file.write(fsobj, arcname=Path(*fsobj.parts[pre_dirs:]))
 
+
+def make_zip_dist(zipfile_path: Path):
+    # pre_dirs determines number of parent directories to remove from the path
+    # Subtract 1 so that we still have a root in the tree
+    pre_dirs = len(zipfile_path.parts) - 1
+    with zipfile.ZipFile(
+        f"{zipfile_path}.zip", "w", zipfile.ZIP_DEFLATED
+    ) as zf:
+        zip_dir(zipfile_path, zf, pre_dirs=pre_dirs)
+
+
+def main():
     spec_files = [
         "MicroscoPyXhair_onedir.spec",
         "MicroscoPyXhair_onefile.spec"
     ]
-    for sf in spec_files:
-        run_pyi(repo_root, sf)
 
-    redist_path.mkdir(exist_ok=True)
-    shutil.copytree(
-        dist_path / "MicroscoPyXhair",
-        redist_path / "MicroscoPyXhair"
+    mpx_dist_path = DIST_PATH / "MicroscoPyXhair"
+    shutil.rmtree(mpx_dist_path)
+
+    for sf in spec_files:
+        run_pyi(REPO_ROOT, sf)
+
+    make_zip_dist(mpx_dist_path)
+
+
+def copy_to_redist():
+    """
+    Create a redist directory and copy the artifacts of the build into it
+    """
+    # TODO Add a argparse function to allow this to be run by user
+    REDIST_PATH.mkdir(exist_ok=True)
+    shutil.copy2(
+        DIST_PATH / "MicroscoPyXhair.zip",
+        REDIST_PATH / "MicroscoPyXhair.zip"
     )
     shutil.copy2(
-        dist_path / "MicroscoPyXhair.exe",
-        redist_path / "MicroscoPyXhair.exe"
+        DIST_PATH / "MicroscoPyXhair.exe",
+        REDIST_PATH / "MicroscoPyXhair.exe"
     )
 
 
